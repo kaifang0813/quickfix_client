c88b7c39f872a04a175afb7be05047159d6b19b2
kai fang <kai@kaidemacbook-pro.local>
2017-12-10 14:23:13 -0500

    Initial Commit

---
+++ quickfix_execturo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  quickfix_execturo
+//
+//  Created by kai fang on 12/10/17.
+//  Copyright © 2017 kai fang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
